7857ab6643a6ed06639d387efcb98f205e3d4177
Alexander Parsan <alexanderparsan28@gmail.com>
2014-07-26 10:07:14 -0400

    Removed DB Files for production

--- db/migrate/20140718175450_morecleanup.rb
+++
@@ -1,9 +0,0 @@
-class Morecleanup < ActiveRecord::Migration
-  def change
-  	drop_table :likes
-  	drop_table :votes
-  	drop_table :likescs
-  	drop_table :like_stores
-  	drop_table :homepages
-  end
-end

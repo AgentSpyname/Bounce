edb16ba8281141f1d093519132ad59657d0f8839
Alexander Parsan <alexanderparsan28@gmail.com>
2014-07-26 10:15:14 -0400

    Fixing bugs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationsController < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
      params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
